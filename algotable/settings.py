d6d54955b9a77def60155d5d8d081de23f6b83cc
Anuj Agarwal <anuj.ism@gmail.com>
2018-04-06 20:08:44 +0530

    add heroku host to allowed hosts

--- algotable/settings.py
+++ algotable/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '!jk3$6igdsh37uw*9=n)jqgwq6*i#f+_8*g(12^gf*l+%x-9ij'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['still-sands-89379.herokuapp.com']
 
 
 # Application definition
